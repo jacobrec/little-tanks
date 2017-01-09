30bab3822e7f298525c78c3f9a54c1a3deb497a1
Peter Elliott <p.elliott332@gmail.com>
2017-01-09 15:48:02 -0700

    added some basic servers stuff

---
+++ server/server.py
@@ -0,0 +1,29 @@
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+from tank import Tank
+
+
+tanks = []
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print("a cleint joined")
+        tanks.append(Tank(
+            self, (0, 0, 0), (0, 0)
+        ))
+
+    def on_message(self, data):
+        pass
+
+    def on_close(self):
+        print("a client left")
+        for tank in tanks:
+            if self is tank.conn:
+                tanks.remove(tank)
+                break
